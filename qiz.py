01c015e0becfbb6c02d87269aab7b2dc94001013
saki10 <rsp10111011@gmail.com>
2022-11-05 17:46:09 +0900

    間違い探しアプリ

---
+++ qiz.py
@@ -0,0 +1,12 @@
+import random
+num3 = random.randint(51,100)
+num4 = random.randint(1,50)
+
+print("問題",num3,"-",num4,"=は?")
+
+ans =  int(input('答えを記入してください'))
+
+if num3 - num4 == ans:
+   print("正解です！")
+else:
+    print("不正解。。")
