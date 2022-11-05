3d33e8852932162f0c0689de3a574b35e8f4e5f8
saki10 <rsp10111011@gmail.com>
2022-11-05 22:34:34 +0900

    おみくじアプリ

--- kanzi.py
+++ kanzi.py
@@ -1,11 +1,12 @@
+
 import random
-kan = random.randint(1,10)
-ji = random.randint(1,10)
-print("撒"*kan +"撤"+"撒"*ji, )
+kan = random.randint(1,10) ##1~10までランダムに出力する　合っている用
+ji = random.randint(1,10)  ##1~10までランダムに出力する　間違い用
+print("撒"*kan +"撤"+"撒"*ji, ) ##間違い探し出力
 
-ans =  int(input('間違っている漢字は何番目？'))
+ans =  int(input('間違っている漢字は何番目？')) ##input関数でユーザが答えを入力できる
 
-if ans == kan + 1:
-   print("正解です！")
+if ans == kan + 1: ##もしあっていた場合は、
+   print("正解です！") ##正解と出力される
 else:
-    print("不正解。。",kan+1,"番目です")
+    print("不正解。。",kan+1,"番目です") #それ以外は、不正解と答えが出力される
---
+++ kurikaesi.py
@@ -0,0 +1,11 @@
+total = 0
+for i in range(5*total) :
+    total = total + i
+print ("合計値は、",total)
+
+
+## 九九の5の段を表示してみよう
+
+for i in range(10):
+    print("5×",i,"=",5*i)
+
