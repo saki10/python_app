bb99c1bc36699254ab6a092f377c3946e1f4b6a2
saki10 <rsp10111011@gmail.com>
2022-11-06 22:28:04 +0900

    誕生石アプリ

---
+++ nest.py
@@ -0,0 +1,6 @@
+#for文の中に、for文を入れる
+
+for i in range(10):
+    for j in range(10):
+        print (i ,"×", j ,"=",i*j)
+
---
+++ omikuji.py
@@ -0,0 +1,4 @@
+import random
+eat = ["肉","魚","野菜","果物"]
+taberu = random.choice(eat)
+print (taberu)
