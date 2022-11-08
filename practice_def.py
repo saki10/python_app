88bfc1278a69eb60619b1773dc6c5c037e2b860c
saki10 <rsp10111011@gmail.com>
2022-11-08 19:20:43 +0900

    関数の練習

--- practice_def.py
+++ practice_def.py
@@ -24,3 +24,19 @@ def split_bills(people,total):
     price = total // people
     print(str(people),'人の場合',str(price),'円です')
 split_bills(10,68900)
+
+
+def get_bills(water,gas,power,rent=8000):
+    total = rent + water +gas + power
+    print (total)
+
+    if total >= 110000 :
+        print('高め')
+    elif total >= 100000 and total < 110000:
+        print('まぁまぁ')
+    else:
+        print('低め')
+get_bills(water=500,gas=500,power=10000)
+
+#実引数に数字を競っているすることができる
+# 未満 <
